67ecd77961bb9a4e21523412fdb120538c8df782
Ayoub Ghriss <dev@aghriss.net>
2023-08-02 03:13:34 +0200

    add cmp config

--- lua/base46/config.lua
+++ lua/base46/config.lua
@@ -11,8 +11,10 @@ return {
 		theme2 = "ayu_light",
 		theme1_icon = " ",
 		theme2_icon = " ",
+
 		-- ~/.config/nvim/lua/custom/
 		custom_themes_path = vim.fn.stdpath("config") .. "/lua/custom",
+
 		-- ~/.local/state/nvim/base46/
 		cache_path = vim.fn.stdpath("state") .. "/base46/",
 		telescope_style = "borderless",
@@ -20,13 +22,13 @@ return {
 		transparency = false,
 		-- highlights to add
 		hl_add = {},
-		-- statusline_theme = "vscode_colored",
-		-- cmp = {
-		-- icons = true,
-		-- lspkind_text = true,
-		-- style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-		-- border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-		-- selected_item_bg = "colored", -- colored / simple
-		-- },
+
+		cmp = {
+			icons = true,
+			lspkind_text = true,
+			style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+			border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+			selected_item_bg = "colored", -- colored / simple
+		},
 	},
 }
